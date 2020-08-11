32bb2c18df20887daefe958b9a3cd9cee28afe56
Mehmet <mlalek@gmail.com>
2020-08-12 00:01:09 +0200

    first

---
+++ src/first_package/firstClass.java
@@ -0,0 +1,13 @@
+package first_package;
+
+
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		 System.out.println("Our first commit");
+		 System.out.println("Our second commit");
+		 System.out.println("Our third commit");
+	}
+
+}
